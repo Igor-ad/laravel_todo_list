00424f08cf3abd15d9d5cfcec04bc9d2915052d7
Igor-ad <admin@autodoctor.od.ua>
2023-08-13 22:47:30 +0300

    Model Task.php - date casting 'completed_at' to 'datetime'

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,6 +10,10 @@ class Task extends Model
 {
     use HasFactory;
 
+    protected $casts = [
+        'completed_at' => 'datetime',
+    ];
+
     /**
      * The attributes that are mass assignable.
      *
