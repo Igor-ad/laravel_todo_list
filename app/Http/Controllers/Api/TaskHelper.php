41ed0191f56116a91c2a3e61df4133f56281c919
Igor-ad <admin@autodoctor.od.ua>
2023-08-14 13:47:57 +0300

    Refactoring Controllers, few fix

--- app/Http/Controllers/Api/TaskHelper.php
+++ app/Http/Controllers/Api/TaskHelper.php
@@ -25,4 +25,23 @@ trait TaskHelper
     {
         return response()->json(data: [$this->ans], status: $this->ans->status);
     }
+
+    /**
+     * @param int $status
+     * @param string $method
+     * @param string $msg
+     * @param mixed $data
+     * @return JsonResponse
+     */
+    protected function getMethod(int $status, string $method, string $msg, mixed $data): JsonResponse
+    {
+            try {
+                $this->ans->status = $status;
+                $this->ans->data = $this->taskService->{$method}($data);
+                $this->ans->message = $msg;
+            } catch (Exception $e) {
+                $this->getCatch($e);
+            }
+            return $this->getJsonResponse();
+    }
 }
