52dcbe8550843b7cce92acf3f9e7f6804542912f
Igor-ad <admin@autodoctor.od.ua>
2023-12-01 20:21:40 +0200

    Refactoring ServiceMapper

--- app/Http/Controllers/ServiceMapper.php
+++ app/Http/Controllers/ServiceMapper.php
@@ -7,20 +7,41 @@ namespace App\Http\Controllers;
 use App\Services\AnswerService;
 use App\Services\ResponseService;
 use Exception;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 
 trait ServiceMapper
 {
-    private function getService($service, $method, $id = null): ResponseService
+    private function get(object $service, string $method, int $id = null): ResponseService
     {
         return $service->$method($id);
+    }
+
+    private function has(object $service, string $method): bool
+    {
+        return method_exists($service, $method);
     }
 
     protected function getAnswer($service, $method, $id = null): AnswerService
     {
         try {
-            $response = $this->getService($service, $method, $id);
+            if ($this->has($service, $method)) {
+                $response = $this->get($service, $method, $id);
 
-            $this->answerService->setAnswer($response);
+                $this->answerService->setAnswer($response);
+            } else {
+                $allowedMethods = get_class_methods($service);
+
+                throw new MethodNotAllowedException(
+                    $allowedMethods,
+                    __('exception.not_allowed_method', [
+                        'method' => $method,
+                        'class' => class_basename($service)
+                    ]) .
+                    __('exception.allowed_methods', [
+                        'methods' => implode(', ', $allowedMethods)
+                    ]),
+                );
+            }
 
         } catch (Exception $e) {
             $this->answerService->setExceptionAnswer($e);
--- resources/lang/en/exception.php
+++ resources/lang/en/exception.php
@@ -3,6 +3,8 @@
 return [
     '404' => "Resource not found. Please check you request. :message",
     'help' => "Please send this error message to your admin, eMail - " . config('app.admin_email'),
+    'not_allowed_method' => "This method - ':method' not allowed on the class ':class'. ",
+    'allowed_methods' => "Allowed methods - :methods",
     'update_transaction_fail' => "Invalid task update Transaction. Error Message - :e.",
     'unauthenticated' => "Unauthorized access. You need to log in to the system!"
 ];
