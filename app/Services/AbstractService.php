56b1622f84531aaae3aaf4891faf69e9ef5f29b4
Igor-ad <admin@autodoctor.od.ua>
2023-12-02 11:16:24 +0200

    Refactoring AbstractService

--- app/Services/AbstractService.php
+++ app/Services/AbstractService.php
@@ -10,11 +10,27 @@ class AbstractService
 {
     public function __call(string $name, array $arguments)
     {
+        $class = class_basename($this);
+
         throw new BadMethodCallException(
-            __('exception.not_allowed_method', [
-                'method' => $name,
-                'class' => class_basename($this)
-            ]),
+            self::badMethodMessage($class, $name),
         );
+    }
+
+    public static function __callStatic(string $name, array $arguments)
+    {
+        $class = class_basename(static::class);
+
+        throw new BadMethodCallException(
+            self::badMethodMessage($class, $name) . ' Static context'
+        );
+    }
+
+    public static function badMethodMessage(string $className, string $method): string
+    {
+        return __('exception.not_allowed_method', [
+            'method' => $method,
+            'class' => $className
+        ]);
     }
 }
