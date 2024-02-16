1d9cc55cb01ee01d3ff18d0665aef87bc08651d1
Igor-ad <admin@autodoctor.od.ua>
2024-02-16 14:51:15 +0200

    fix factories/TaskFactory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -20,7 +20,7 @@ class TaskFactory extends Factory
     {
 
         return [
-            'parent_id' => Task::all()->last()->id,
+            'parent_id' => Task::all()->last()->id ?? 1,
             'user_id' => 1,
             'status' => TaskStatusEnum::TODO->value,
             'priority' => rand(1, 5),
